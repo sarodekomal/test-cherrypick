1a78db54c4b846bb0eb5e52e09f607571ff81286
sarodekomal <Komal.Sarode@extentia.com>
2017-04-17 14:36:30 +0530

    Project added to features branch

---
+++ CoreWebAPIExample/src/CoreWebAPIExample/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CoreWebAPIExample.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
